0e170a62d7f9f253a370260e1690f9b51adbd9b0
andrehen27 <andre.hendrawan27@yahoo.com>
2019-01-10 12:10:17 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var routes = require('./routes');
+//home
+router.get('/', routes.home)
+
+//movie_number
+router.get('/harry_potter_chapter/:movie_number?', routes.movie_number)
+
+module.exports = router;
