20caf8c1faeac0a815b1cf0ac840313034629976
andrehen27 <andre.hendrawan27@yahoo.com>
2019-01-14 10:10:49 +0700

    UNDO

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,8 @@ var express = require('express');
 var router = express.Router();
 var routes = require('./routes');
 
-//login
-router,get('/',router.login)
-
 //home
-router.get('/home', routes.home)
+router.get('/', routes.home)
 
 //movie_number
 router.get('/harry_potter_chapter/:movie_number?', routes.movie_number)
